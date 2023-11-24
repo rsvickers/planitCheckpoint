4ffab2dc1d387973d01b550bea586655b3c778be
Rhys Vickers <rs.vickers5@gmail.com>
2023-11-24 15:05:24 -0700

    All services made

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg  px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <h1>Planit</h1>
       </div>
     </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
