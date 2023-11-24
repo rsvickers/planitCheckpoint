b0f573971a2d14b68e368f7b06427f30df9b0c08
Rhys Vickers <rs.vickers5@gmail.com>
2023-11-24 16:01:36 -0700

    just working

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,11 +1,47 @@
 <template>
-  <div></div>
+  <div class="container-fluid">
+    <section class="row box">
+      <div class="col-12 d-flex justify-content-between p-5 ">
+        <div>
+          <h5 class="funColor">
+            Projects
+          </h5>
+          <p>
+            a list of projects for {{ account.name }}
+          </p>
+        </div>
+        <div>
+          <button class="btn btn-outline-info funColor">
+            Create Project
+          </button>
+        </div>
+      </div>
+      <div class="col-12 text-center funColor">
+        <section class="row">
+          <div class="col-4">
+            <p>Name</p>
+          </div>
+          <div class="col-4 underline">
+            <p>Members</p>
+          </div>
+          <div class="col-4">
+            <p>Started</p>
+          </div>
+        </section>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+
 export default {
   setup() {
-    return {}
+    return {
+      account: computed(() => AppState.account)
+    }
   }
 }
 </script>
@@ -29,5 +65,19 @@ export default {
       object-position: center;
     }
   }
+}
+
+.funColor {
+  color: rgb(242, 11, 204);
+}
+.underline {
+  border-bottom: 2px solid grey;
+}
+
+.box {
+  max-width: 85%;
+  max-height: 77vh;
+  box-shadow: 0 5px 7px grey;
+  border-radius: 13px;
 }
 </style>
